991ade4fcf8e17d19d7300af2b093cf64836ffda
Tài Ngô Bá Tấn <taingo2112003@gmail.com>
2025-02-22 19:47:53 +0700

    Modified API_URL with deployed domain of backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:8080/tasks";
+const API_URL = "https://backendapiapp-production.up.railway.app/tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
